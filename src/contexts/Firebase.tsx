c39995850d262cf1365a677db3a2b672b87e04bc
CaptKillSwitch <62815232+CaptKillSwitch@users.noreply.github.com>
2022-12-09 15:06:27 -0500

    switching back to react

---
+++ src/contexts/Firebase.tsx
@@ -0,0 +1,44 @@
+// Import the functions you need from the SDKs you need
+import * as React from 'react';
+import {ReactNode} from 'react';
+import {FirebaseApp, getApp, getApps, initializeApp} from "firebase/app";
+import {connectAuthEmulator, getAuth} from "firebase/auth";
+import {connectFirestoreEmulator, getFirestore} from "firebase/firestore";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_REALTIME_DATABASE,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+function getFirebaseApp(): FirebaseApp {
+    const firebaseApp = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+    const auth = getAuth(firebaseApp);
+    const db = getFirestore(firebaseApp);
+    if (!process.env.REACT_APP_SERVICE_MODE || process.env.REACT_APP_SERVICE_MODE === 'development') {
+        connectAuthEmulator(auth, "http://localhost:9099");
+        connectFirestoreEmulator(db, 'localhost', 8080);
+    }
+
+    return firebaseApp;
+}
+
+export const FirebaseContext = React.createContext<FirebaseApp | undefined>(undefined);
+
+function FirebaseContextProvider(props: { children: ReactNode }) {
+
+    return (
+        <FirebaseContext.Provider value={getFirebaseApp()}>
+            {props.children}
+        </FirebaseContext.Provider>
+    );
+}
+
+export default FirebaseContextProvider;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider,} from "react-router-dom";
+import FirebaseContextProvider from "./contexts/Firebase";
+import Home from "./routes/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import Navbar from "./components/Navbar/Navbar";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home/>,
+    },
+])
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <FirebaseContextProvider>
+            <Navbar/>
+            <RouterProvider router={router}/>
+        </FirebaseContextProvider>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
